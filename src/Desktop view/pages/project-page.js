3b1ac7b1db3c1f8f301fda72def1d1580d18b4a4
Arshia Naseri <arshia.naseri.w@outlook.com>
2024-08-27 17:04:18 -0400

    + scroll action

--- src/Desktop view/pages/project-page.js
+++ src/Desktop view/pages/project-page.js
@@ -3,16 +3,17 @@ import FloppyDisk from "../../Componants/_floppyDisk";
 
 const ProjectPage = ({ projectsSectionRef }) => {
   const projectCarouselRef = useRef();
+  const btnNextProject = useRef();
+  const btnPreProject = useRef();
 
   const getScrollPercentage = (elm) => {
     return (elm.scrollLeft / (elm.scrollWidth - elm.clientWidth)) * 100;
   };
+
   const isProjectInView = (
     projectElm,
     parentElm = projectCarouselRef.current
   ) => {
-    const viewError = 0;
-
     let leftValue =
       projectElm.getBoundingClientRect().left -
       parentElm.getBoundingClientRect().left;
@@ -33,16 +34,9 @@ const ProjectPage = ({ projectsSectionRef }) => {
     return false;
   };
 
-  const nextProjectPage = () => {
+  const nextProjectPage = (e) => {
     let alreadyPushed = false;
     let pushLeftValue;
-    if (getScrollPercentage(projectCarouselRef.current) > 75) {
-      pushLeftValue =
-        projectCarouselRef.current.scrollWidth -
-        projectCarouselRef.current.clientWidth;
-
-      alreadyPushed = true;
-    }
 
     for (
       let index = 0;
@@ -72,6 +66,50 @@ const ProjectPage = ({ projectsSectionRef }) => {
     });
   };
 
+  const preProjectPage = (e) => {
+    let alreadyPushed = false;
+    let pushLeftValue;
+
+    for (
+      let index = projectCarouselRef.current.children.length - 1;
+      index > -1 && !alreadyPushed;
+      index--
+    ) {
+      let currentProject = projectCarouselRef.current.children[index];
+      if (
+        isProjectInView(currentProject) ||
+        currentProject.getBoundingClientRect().left > 0
+      ) {
+        continue;
+      }
+
+      pushLeftValue =
+        currentProject.getBoundingClientRect().left -
+        projectCarouselRef.current.getBoundingClientRect().left +
+        projectCarouselRef.current.scrollLeft;
+
+      break;
+    }
+
+    projectCarouselRef.current.scrollTo({
+      left: pushLeftValue,
+      behavior: "smooth",
+    });
+  };
+
+  const changeBtnOpacity = (e) => {
+    if (getScrollPercentage(e.currentTarget) === 0) {
+      btnPreProject.current.style.opacity = 0.4;
+      btnNextProject.current.style.opacity = 1;
+    } else if (getScrollPercentage(e.currentTarget) === 100) {
+      btnNextProject.current.style.opacity = 0.4;
+      btnPreProject.current.style.opacity = 1;
+    } else {
+      btnPreProject.current.style.opacity = 1;
+      btnNextProject.current.style.opacity = 1;
+    }
+  };
+
   return (
     <>
       <section
@@ -82,13 +120,24 @@ const ProjectPage = ({ projectsSectionRef }) => {
           <div className=" font-vcr text-4xl tracking-[1rem]">PROJECTS</div>
           <section className=" bg-green-400 flex flex-col ">
             <section className=" flex text-7xl gap-1 ml-auto mr-5 font-vcr noHighlightClicked">
-              <div className=" cursor-pointer opacity-40">◄</div>
-              <div onClick={nextProjectPage} className=" cursor-pointer">
+              <div
+                ref={btnPreProject}
+                onClick={preProjectPage}
+                className=" cursor-pointer opacity-40"
+              >
+                ◄
+              </div>
+              <div
+                ref={btnNextProject}
+                onClick={nextProjectPage}
+                className=" cursor-pointer"
+              >
                 ►
               </div>
             </section>
             <section
               ref={projectCarouselRef}
+              onScroll={changeBtnOpacity}
               className=" flex gap-16 w-full overflow-scroll"
             >
               <FloppyDisk
