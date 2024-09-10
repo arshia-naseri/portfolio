8e1928d253ac26bed6754425e4eeb42f5f93d0bc
Arshia Naseri <arshia.naseri.w@outlook.com>
2024-09-10 12:18:09 -0400

    * Navbar color changed Mobile

--- src/Mobile view/mobile.js
+++ src/Mobile view/mobile.js
@@ -1,20 +1,27 @@
 import { useRef } from "react";
 import MainPage from "./pages/main-page";
+import ProjectsPage from "./pages/projects-page";
 
 const Mobile = () => {
   const mainSectionRef = useRef();
-  //   const projectsSectionRef = useRef();
+  const projectsSectionRef = useRef();
   //   const aboutSectionRef = useRef();
   //   const arcadeSectionRef = useRef();
   return (
     <>
       <main>
-        <div className="fixed z-20 m-2 cursor-pointer text-4xl text-mainWhite blur-[.5px]">
+        <div
+          id="btnBurgerMenu"
+          className="fixed z-20 m-2 cursor-pointer text-4xl text-mainWhite blur-[.5px]"
+        >
           ☰
         </div>
 
         <section>
           <MainPage mainSectionRef={mainSectionRef} />
+          <ProjectsPage projectsSectionRef={projectsSectionRef} />
+          <section className="h-dvh bg-red-200"></section>
+          <section className="h-dvh bg-blue-200"></section>
         </section>
       </main>
     </>
---
+++ src/Mobile view/pages/projects-page.js
@@ -0,0 +1,14 @@
+const ProjectsPage = ({ projectsSectionRef }) => {
+  return (
+    <>
+      <section
+        ref={projectsSectionRef}
+        className="flex h-dvh border-b border-vcrBlack/20 bg-mainWhite font-vcr"
+      >
+        <main></main>
+      </section>
+    </>
+  );
+};
+
+export default ProjectsPage;
